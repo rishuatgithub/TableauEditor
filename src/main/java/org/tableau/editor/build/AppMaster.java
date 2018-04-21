191c040935ded9f1bcd6dcd9d389bdbbc334bad6
Rishu Shrivastava <rishu.shrivastava@gmail.com>
2018-04-21 07:20:10 +0530

    Buidling the Framework

--- src/main/java/org/tableau/editor/build/AppMaster.java
+++ src/main/java/org/tableau/editor/build/AppMaster.java
@@ -1,37 +1,134 @@
 package org.tableau.editor.build;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
-public class AppMaster {
+public class AppMaster extends JFrame implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3492701885097648184L;
+	
+	JFrame jf;
+	JButton browsebtn, okbtn, cancelbtn;
+	JTextField textfield, connectionFrom, connectionTo;
+	JTextArea fileslist;
+	JLabel dirlistLbl, connectionFromLbl, connectionToLbl;
+	
+	public AppMaster() {
+		
+		jf = new JFrame("Tableau Editor");
+		
+		browsebtn = new JButton("Select Directory");
+		browsebtn.addActionListener(this);
+		browsebtn.setBounds(10, 10, 150, 30);
+		
+		okbtn = new JButton("Start Proceesing");
+		okbtn.addActionListener(this);
+		
+		cancelbtn = new JButton("Reset");
+		cancelbtn.addActionListener(this);
+		
+		textfield = new JTextField();
+		textfield.setMinimumSize(browsebtn.getMinimumSize());
+		textfield.setBounds(10, 10, 200, 30);
+		
+		fileslist = new JTextArea();
+		fileslist.setEditable(false);
+		JScrollPane scroll = new JScrollPane(fileslist);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+				
+		dirlistLbl = new JLabel("List of files to be processed");
+				
+		connectionFromLbl = new JLabel("Enter the Current Server Details");
+		connectionFrom = new JTextField();
+		
+		connectionToLbl = new JLabel("Enter the New Server Details");
+		connectionTo = new JTextField();
+		
+		jf.add(browsebtn);
+		jf.add(textfield);
+		
+		jf.add(dirlistLbl);
+		jf.add(scroll);
+		jf.add(connectionFromLbl);
+		jf.add(connectionFrom);
+		jf.add(connectionToLbl);
+		jf.add(connectionTo);
+		jf.add(okbtn);
+		jf.add(cancelbtn);
+		jf.setLayout(new GridLayout(5, 2));
+		
+		jf.setVisible(true);
+		jf.setSize(600, 500);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
 
 	public static void main(String[] args) {
-		
-		JFrame jf = new JFrame("Tableau Editor");
-		
-		final JTextField tf = new JTextField();
-		tf.setBounds(50,50,100,40);
-		
-		JButton jb = new JButton("Click");		
-		jb.setBounds(130, 100, 100, 40);		
-		
-		jb.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				tf.setText("Hello Rishu");
+		new AppMaster();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == browsebtn) {
+			JFileChooser fc = new JFileChooser();
+			int returnval = fc.showOpenDialog(browsebtn);
+			fc.setDialogTitle("Select Directory");
+			fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+			fc.setAcceptAllFileFilterUsed(false);
+			
+			if(returnval == JFileChooser.APPROVE_OPTION) {
+				File f = fc.getCurrentDirectory();
+				//System.out.println(f.getAbsolutePath());
+				textfield.setText(f.getAbsolutePath());
+				
+				File dir = new File(f.getAbsolutePath());
+				File[] filelist = dir.listFiles();
+				String filetextarea = " ";
+				
+				for (File files: filelist) {
+					System.out.println(files);
+					filetextarea += files.getName()+"\n";
+				}
+				fileslist.setText(filetextarea);
 			}
-		});
+		}
+		
+		/*if(e.getSource() == okbtn) {
+			System.out.println("List all the files in the directory and start processing");
+			
+			File dir = new File(textfield.getText());
+			File[] filelist = dir.listFiles();
+			String filetextarea = " ";
+			
+			for (File files: filelist) {
+				System.out.println(files);
+				filetextarea += files.getName()+"\n";
+			}
+			fileslist.setText(filetextarea);
+			
+		}*/
+		
+		if(e.getSource() == cancelbtn) {
+			textfield.setText(null);
+			fileslist.setText(null);
+			connectionFrom.setText(null);
+			connectionTo.setText(null);
+		}
 		
 		
-		jf.add(jb);
-		jf.add(tf);		
-		
-		jf.setSize(400, 400);
-		jf.setLayout(null);
-		jf.setVisible(true);
 	}
 
 }
